a1f10e88fe15dfc229b4533e0d1b2cac872832b3
Bohdan Suprun <bohdan.suprun@advanced-solutions.us>
2024-07-22 11:10:27 +0000

    Fixed min password characters

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -14,7 +14,7 @@ class AuthController extends Controller
         $request->validate([
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
-            'password' => 'required|string|min:8',
+            'password' => 'required|string|min:4',
         ]);
 
         $user = User::create([
